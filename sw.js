c96a53964a00d1d0c6d85da456b78b17757d8ec3
Hong-anson <shuai830302@gmail.com>
2026-05-19 02:47:09 +0000

    test

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
-const CACHE_NAME = 'nz-quiz-v1';
+const CACHE_NAME = 'nz-quiz-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './bg.jpg'
 ];
 
-// 安裝時快取檔案
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 斷網時攔截請求，提供快取內容
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
